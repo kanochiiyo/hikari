847c199f18e946ef5636ea81c55cab0a3ec4994a
kanochiiyo <124230084@student.upnyk.ac.id>
2025-08-19 20:50:39 +0700

    add navbar and hero

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include(__DIR__ . "/templates/header.php");
+include(__DIR__ . "/templates/navbar.php");
+?>
+
+<main>
+  <!-- Hero -->
+  <section class="hero" id="hero">
+    <div class="container d-flex align-items-center justify-content-center fs-1 text-white flex-column">
+      <h1>Hello world</h1>
+    </div>
+  </section>
+  <!-- End Hero -->
+
+
+</main>
+
+
+
+
+<?php
+include(__DIR__ . "/templates/footer.php");
+?>
